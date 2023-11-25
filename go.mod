21ebb00c0041d261f8694514a26bbcd235a3ae55
Ripio <ripioconarcilla@gmail.com>
2023-11-25 11:30:02 -0300

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example-server
+
+go 1.21.4
+
+require google.golang.org/grpc v1.59.0
---
+++ server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+    "github.com/sysinternals/example-grpc-server/userpb"
+	"google.golang.org/grpc"
+)
+
+type userServiceServer struct{}
+
+func (s *userServiceServer) GetUserByID(ctx context.Context, req *user.UserRequest) (*user.UserResponse, error) {
+	// In a real application, you would fetch user data from a database based on the user ID
+	// For simplicity, we'll return a dummy user here
+	return &user.UserResponse{
+		UserId:   req.UserId,
+		Username: "john_doe",
+		Email:    "john@example.com",
+	}, nil
+}
+
+func main() {
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	user.RegisterUserServiceServer(grpcServer, &userServiceServer{})
+
+	fmt.Println("gRPC Server is listening on :8080...")
+	err = grpcServer.Serve(listen)
+	if err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
